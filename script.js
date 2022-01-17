cc6f1e7711c0473020b1ec786a03634bc7c3f684
Divyanka02 <85795778+Divyanka02@users.noreply.github.com>
2022-01-17 18:35:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,176 @@
+/*var drawing = false;
+var context;
+
+window.onload=function()
+{
+    //Clear Button
+    document.getElementById('btnClear').addEventListener('click', function(){
+            context.clearRect(0,0, context.canvas.width, context.canvas.height);       
+        }, false);
+    
+    //Back Button
+    document.getElementById('btnBack').addEventListener('click', function(){
+            document.getElementById('myCanvas').style.display = "block";
+            document.getElementById('saveArea').style.display = "none";
+            document.getElementById('tools').style.display = "block";
+            
+        }, false);
+    
+    //Width Scale
+    document.getElementById('lineWidth').addEventListener('change', function(){
+            context.lineWidth = document.getElementById('lineWidth').value;
+        }, false);
+    
+    //Color
+    document.getElementById('colorChange').addEventListener('change', function(){
+            context.strokeStyle = document.getElementById('colorChange').value;
+        }, false);
+    
+    //Save
+    document.getElementById('btnSave').addEventListener('click', function(){
+            document.getElementById('myCanvas').style.display = "none";
+            document.getElementById('saveArea').style.display = "block";
+            document.getElementById('tools').style.display = "none";
+            
+            var dataURL = document.getElementById('myCanvas').toDataURL();
+            document.getElementById('canvasImg').src = dataURL;
+        }, false);
+    
+    //Size Canvas
+    context = document.getElementById('myCanvas').getContext("2d");
+    context.canvas.width = window.innerWidth;
+    context.canvas.height = window.innerHeight-60;
+    
+    //Mouse movement
+    document.onmousemove = handleMouseMove;
+    document.onmousedown = handleDown;
+    document.onmouseup = handleUp;
+    
+    //Style line
+    context.strokeStyle = "#000";
+    context.lineJoin = "round";
+    context.lineWidth = 5;
+    
+    //Hide Save Area
+    document.getElementById('saveArea').style.display = "none";
+}
+
+function handleMouseMove(e)
+{
+    console.log(e.clientX);
+    console.log(e.clientY);
+    if(drawing)
+    {
+       
+        context.lineTo(e.clientX, e.clientY);
+        context.closePath();
+        context.stroke();
+        context.moveTo(e.clientX, e.clientY);
+    } else
+    {
+        context.moveTo(e.clientX, e.clientY);
+    }
+    
+}
+
+function handleDown(e)
+{
+    drawing = !drawing; 
+    console.log(drawing);
+    context.moveTo(e.clientX, e.clientY);
+    context.beginPath();
+     
+}
+
+function handleUp()
+{
+    drawing = !drawing;
+}
+/*
+
+*/
+
+var drawing=false;
+var context;
+
+window.onload = function(){
+     //Back Button
+     document.getElementById('btnBack').addEventListener('click', function(){
+        document.getElementById('myCanvas').style.display = "block";
+        document.getElementById('savearea').style.display = "none";
+        document.getElementById('tools').style.display = "block";
+        
+    }, false);
+    //claer button
+   document.getElementById('btnClear').addEventListener('click',function(){
+        context.clearRect(0,0, context.canvas.width, context.canvas.height);  
+    },false);
+
+    //Line width
+    document.getElementById('lineWidth').addEventListener('change',function(){
+        context.lineWidth=document.getElementById('lineWidth').value;
+    },false);
+
+//color
+document.getElementById('colorChange').addEventListener('change',function(){
+    context.strokeStyle=document.getElementById('colorChange').value;
+},false);
+
+
+ //Save
+ document.getElementById('btnSave').addEventListener('click', function(){
+    document.getElementById('myCanvas').style.display = "none";
+    document.getElementById('savearea').style.display = "block";
+    document.getElementById('tools').style.display = "none";
+    
+    var dataURL = document.getElementById('myCanvas').toDataURL();
+    document.getElementById('canvasImg').src = dataURL;
+}, false);
+
+
+    //size canvas
+    context = document.getElementById('myCanvas').getContext("2d");
+    context.canvas.width = window.innerWidth;
+    context.canvas.height = window.innerHeight-60;
+
+    //mouse movement
+    document.onmousemove= handleMouseMove;
+    document.onmousedown= handleDown;
+    document.onmouseup =handleUp;
+
+    //styleline
+
+    context.strokeStyle="black";
+    context.lineJoin="round";
+    context.lineWidth="3";
+
+
+}
+
+function handleMouseMove(e){
+    console.log(e.clientX,e.clientY);
+    if(drawing)
+    {
+        context.lineTo(e.clientX,e.clientY);
+        context.closePath();
+        context.stroke();
+        context.moveTo(e.clientX,e.clientY);
+    }
+    else{
+        context.moveTo(e.clientX,e.clientY);
+    }
+}
+
+function  handleDown(e){
+
+    drawing =!drawing;
+    console.log(drawing);
+    context.moveTo(e.clientX,e.clientY);
+    context.beginPath();
+
+}
+function handleUp(e){
+    drawing =!drawing;
+    console.log(drawing);   
+};
+
